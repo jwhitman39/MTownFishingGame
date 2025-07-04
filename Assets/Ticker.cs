e4ac3c8f768cb08e1523dbb7e054ff6af5ff621b
jwhitman39 <jtw3909@gmail.com>
2025-07-03 18:50:47 -0700

    EOD commit 7/3
    refined code, added beginning of UI, reel works as intended

--- Assets/Ticker.cs
+++ Assets/Ticker.cs
@@ -1,4 +1,6 @@
 using System.Drawing;
+using System.Net.Sockets;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.UI.Image;
@@ -15,6 +17,7 @@ public class Ticker : MonoBehaviour
     public bool topLever = false;
     public bool bottomLever = false;
     public GameObject spoolEnd;
+    public GameObject reelAmount;
     float length = 0.4f;
 
 
@@ -26,12 +29,13 @@ public class Ticker : MonoBehaviour
 
     void FixedUpdate()
     {
+        reelAmount.GetComponent<TextMeshProUGUI>().text = fishingScore.ToString();
         Debug.DrawRay(spoolEnd.transform.position, Vector3.left * length, rayColor);
         Debug.DrawRay(spoolEnd.transform.position, Vector3.right * length, rayColor2);
         // Cast a ray straight down.
         hit = Physics2D.Raycast(spoolEnd.transform.position, Vector2.left, length, LayerMask.GetMask("FishingRod"));
         hit2 = Physics2D.Raycast(spoolEnd.transform.position, Vector2.right, length, LayerMask.GetMask("FishingRod"));
-        Debug.Log("hit is " + hit + "and hit.collider is " + hit.collider);
+        //Debug.Log("hit is " + hit + "and hit.collider is " + hit.collider);
         if (hit && hit.collider.name == "Lever")
         {
             Debug.Log("successful hit.");
@@ -43,6 +47,7 @@ public class Ticker : MonoBehaviour
             Debug.Log("successful hit up top.");
             hit2.collider.attachedRigidbody.MoveRotation(100);
             topLever = true; 
+            bottomLever= false;
         }
         if (bottomLever && topLever)
         {
@@ -52,7 +57,12 @@ public class Ticker : MonoBehaviour
     }
     void AddScore()
     {
+            topLever = false; 
             fishingScore++;
-            Debug.Log(fishingScore);
+            Debug.Log("you have reeled in " + fishingScore + " times");
     }
+    //void CheckScore()
+    // a function that will crosscheck between your reel-in score and the amount required for the specific fish
+    //{
+    //}
 }
